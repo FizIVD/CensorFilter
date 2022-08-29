33f12163ff75c9f4e2c8b03dcded50c37d46f26d
python <none@none.none>
2022-08-29 23:14:03 +0500

    Fix comments and tests.py

--- censor_app/tests.py
+++ censor_app/tests.py
@@ -1,14 +1,15 @@
 from django.test import TestCase
 
-from censor_app.views import censor_filter
+from censor_app.views import censor_filter, MethodType
+
 
 class Tests(TestCase):
     def test_filter(self):
-        result = censor_filter("Привет сучара", "fast")[0]
+        result = censor_filter("Привет сучара", MethodType.fast)[0]
         self.assertEqual(result, "Привет ******")
 
-        result = censor_filter("м о з г о е б", "deep")[0]
-        self.assertEqual(result, "м о з г о ***")
+        result = censor_filter("м о з г о е б", MethodType.deep)[0]
+        self.assertEqual(result, "м о з г *****")
 
-        result = censor_filter("Не пили сук, подстрахуйся", "deep")[0]
+        result = censor_filter("Не пили сук, подстрахуйся", MethodType.deep)[0]
         self.assertEqual(result, "Не пили сук, подстрахуйся")
